16d38b1ce83ec7e7afc5485e938707ed502943ce
senekit <1792700051@qq.com>
2021-11-05 18:23:37 +0800

    完成读取csv的操作

--- experiment1/main.py
+++ experiment1/main.py
@@ -4,6 +4,8 @@ import numpy as np
 import os
 
 target_data = [[] for i in range(18)]
+X_train = []
+Y_train = []
 
 
 def read_csv(file_path: str) -> None:
@@ -20,7 +22,31 @@ def read_csv(file_path: str) -> None:
             count += 1
 
 
+# def handle_data() -> None:
+#     for month in range(12):
+#         for group in range(471):
+#             X_train.append([])
+#             for attribute in range(18):
+#                 for hour in range(9):
+#                     X_train[month*471 + group].append(target_data[attribute][month * 480 + group + hour])
+#             Y_train.append(target_data[9][month * 480 + group + 9])
+#     X_train = np.array(X_train)
+#     Y_train = np.array(Y_train)
+#
+
 if __name__ == '__main__':
     file_path = '../data1/train.csv'
     read_csv(file_path=file_path)
     print(np.array(target_data).shape)
+    X_train = []
+    Y_train = []
+    for month in range(12):
+        for group in range(471):
+            X_train.append([])
+            for attribute in range(18):
+                for hour in range(9):
+                    X_train[month * 471 + group].append(target_data[attribute][month * 480 + group + hour])
+            Y_train.append(target_data[9][month * 480 + group + 9])
+    X_train = np.array(X_train)
+    Y_train = np.array(Y_train)
+    print(np.array(X_train).shape)
