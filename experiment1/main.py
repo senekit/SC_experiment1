71d85f28779370d874624e31c9aa0867902298b5
senekit <1792700051@qq.com>
2021-11-05 20:25:36 +0800

    FEAT: 完成实验一

--- experiment1/main.py
+++ experiment1/main.py
@@ -1,4 +1,5 @@
 import csv
+import math
 
 import numpy as np
 import os
@@ -8,8 +9,8 @@ X_train = []
 Y_train = []
 
 
-def read_csv(file_path: str) -> None:
-    with open(file_path, 'r') as target_csv:
+def read_csv() -> None:
+    with open('../data1/train.csv', 'r', encoding='gb18030') as target_csv:
         target_reader = csv.reader(target_csv)
         target_reader.__next__()
         count = 0
@@ -36,7 +37,7 @@ def read_csv(file_path: str) -> None:
 
 if __name__ == '__main__':
     file_path = '../data1/train.csv'
-    read_csv(file_path=file_path)
+    read_csv()
     print(np.array(target_data).shape)
     X_train = []
     Y_train = []
@@ -50,3 +51,31 @@ if __name__ == '__main__':
     X_train = np.array(X_train)
     Y_train = np.array(Y_train)
     print(np.array(X_train).shape)
+    # iteration = 100
+    # weights = np.ones(18 * 9)
+    # learning_rate = 0.01
+    # gra_2_sum = np.ones(18 * 9)
+    # for i in range(iteration):
+    #     gradient = np.zeros(18 * 9)
+    #     variance = 0
+    #     for j in range(len(X_train)):
+    #         loss = np.dot(weights, X_train[j]) - Y_train[j]
+    #         variance += loss ** 2
+    #         gradient = 2 * np.dot(X_train[j].T, loss)
+    #         gra_2_sum += gradient ** 2
+    #         ada = np.sqrt(gra_2_sum + 1e-8)
+    #         weights -= learning_rate * gradient / ada
+    #     if i % 10 == 0:
+    #         cost = variance / len(X_train)
+    #         cost_a = math.sqrt(cost)
+    #         print(i, " times ", " variance: ", cost, " standard deviation: ", cost_a)
+    # np.save('model.npy', 'w')
+    w = np.load('model.npy')
+    text = open('../data1/test.csv', 'r')
+    row = csv.reader(text, delimiter=',')
+    pre_list = []
+    for i in range(len(X_train)):
+        pre_result = np.dot(weights, X_test[i])
+        pre_list.append(pre_result)
+
+
